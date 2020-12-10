ff3c54c32e4a416933e95a73ae6e5334838dcbc1
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-12-10 22:12:15 +0700

    Slider Horizontal

---
+++ jQuery Gallery Photo - Sekolah Koding/#003 Slider Horizontal Vertikal/Horizontal/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    function slide(num){
+        if( $('.container').css('marginLeft') == '-1600px')
+            $('.container').css('marginLeft', '0px');
+        else
+            $('.container').animate({marginLeft: num})
+    }
+    var autoSlide = setInterval(function(){
+        slide('-=400px');
+    }, 2000);
+
+    $('#next').click(function(){
+        slide('-=400px');
+        clearInterval(autoSlide);
+    });
+
+    $('#prev').click(function(){
+        slide('+=400px');
+        clearInterval(autoSlide);
+    });
+});
